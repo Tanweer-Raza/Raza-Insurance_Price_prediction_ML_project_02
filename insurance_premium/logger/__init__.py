f0ed78b79e976facf4d22030962cd8ecc039b89b
Tanweer Raza <tanatif@gmail.com>
2022-09-27 16:05:29 +0530

    Completed Stage I

--- insurance_premium/logger/__init__.py
+++ insurance_premium/logger/__init__.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from insurance_premium.constant import get_current_time_stamp
 import os
 
-LOG_DIR="insurance_logs"
+LOG_DIR="logs"
  
 def get_log_file_name():
     return f"log_{get_current_time_stamp()}.log" 
@@ -29,7 +29,7 @@ def get_log_dataframe(file_path):
     data=[]
     with open(file_path) as log_file:
         for line in log_file.readlines():
-            data.append(line.split(" ^; "))
+            data.append(line.split("^;"))
 
     log_df = pd.DataFrame(data)
     columns=["Time stamp","Log Level","line number","file name","function name","message"]
